f6fa5ee975066147263abdcde7d5fe4cdf19b9ff
alena <158552163+alyonkanezelenka@users.noreply.github.com>
2024-05-24 04:31:06 +0300

    Add files via upload

---
+++ laba 2-1.cpp
@@ -0,0 +1,16 @@
+﻿//Дано натуральное число n.Выведите все числа от 1 до n.
+#include <iostream>
+using namespace std;
+void printNumbers(int n) {
+    if (n > 0) {
+        printNumbers(n - 1);
+        cout << n << endl;
+    }
+}
+int main() {
+    int n;
+    cout << "Enter a natural number:";
+    cin >> n;
+    printNumbers(n);
+    return 0;
+}
