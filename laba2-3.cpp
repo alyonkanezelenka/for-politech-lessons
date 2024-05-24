a386735618e4a351c5b257801cc6ff71ab689fb4
alena <158552163+alyonkanezelenka@users.noreply.github.com>
2024-05-24 04:38:22 +0300

    Add files via upload

---
+++ laba2-3.cpp
@@ -0,0 +1,20 @@
+﻿//Дано натуральное число N.Вычислите сумму его цифр.
+//При решении этой задачи нельзя использовать строки, списки, массивы(ну и циклы, разумеется).
+#include <iostream>
+using namespace std;
+int sumDigits(int n, int sum = 0) {
+    if (n == 0) {
+        return sum;
+    }
+    else {
+        int digit = n % 10;
+        return sumDigits(n / 10, sum + digit);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter a natural number: ";
+    cin >> n;
+    cout << "Sum of digits: " << sumDigits(n) << endl;
+    return 0;
+}
