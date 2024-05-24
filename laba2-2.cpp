52c44a2cb822b5aa8cf1569fe3af62cc43d72595
alena <158552163+alyonkanezelenka@users.noreply.github.com>
2024-05-24 04:35:35 +0300

    Add files via upload

---
+++ laba2-2.cpp
@@ -0,0 +1,26 @@
+﻿//Даны два целых числа A и В(каждое в отдельной строке).
+//Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае
+#include <iostream>
+using namespace std;
+void printNumbers(int a, int b) {
+    if (a == b) {
+        cout << a << endl;
+    }
+    else if (a < b) {
+        cout << a << endl;
+        printNumbers(a + 1, b);
+    }
+    else {
+        cout << a << endl;
+        printNumbers(a - 1, b);
+    }
+}
+int main() {
+    int a, b;
+    cout << "Enter A: ";
+    cin >> a;
+    cout << "Enter B: ";
+    cin >> b;
+    printNumbers(a, b);
+    return 0;
+}
